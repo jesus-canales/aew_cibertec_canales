f5593424a0cbe0b3979b1c0b1ae34a20863011ef
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2025-02-12 01:52:44 +0000

    Ejercicio resaltado

--- 03_JS/01_resaltar/js/app.js
+++ 03_JS/01_resaltar/js/app.js
@@ -1,2 +1,15 @@
-var nombre = prompt("Ingresa lugar de procedencia:");
-alert("Hola "+ nombre);
+
+var miSubtitulo = document.getElementById('subtitulo');
+
+miSubtitulo.addEventListener('click', function(){
+    console.log('haz hecho click');
+});
+
+var heroesAvengers = document.querySelectorAll('.listAvenger'); // 6 elementos
+for (i = 0; i < heroesAvengers.length; i++) {
+    heroesAvengers[i].addEventListener('click', colorRojo);
+};
+
+function colorRojo () {
+    avengerSeleccionado = this.classList.toggle('red');
+};
